b5998a5815d8321dd4fd4742217dec941622237b
Charles Mariot (42) <contact@charles-mariot.fr>
2024-02-02 14:55:22 +0100

    Python 3.10.13, opencv 4.9.0.80, plantcv 4.2.1

--- transformation/Transformation.py
+++ transformation/Transformation.py
@@ -4,12 +4,9 @@ import argparse
 from plantcv import plantcv as pcv
 import plantcv as pcv2
 import os
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
 import seaborn as sns
-=======
 
->>>>>>> a20a41b9c81a79f1c033613af585560919c7cb4f
 
 def parse_argument() -> str:
 
@@ -90,72 +87,48 @@ def parse_argument() -> str:
     )
 
 
+def plot_stat_hist(label, sc=1):
+
+    y = pcv.outputs.observations['default_1'][label]['value']
+    x = [i * sc for i in  pcv.outputs.observations['default_1'][label]['label']]
+
+    if label == "hue_frequencies":
+        x = x[:int(255 / 2)]
+        y = y[:int(255 / 2)]
+    if label == "blue-yellow_frequencies" or label == "green-magenta_frequencies":
+        x = [x + 128 for x in x]
+
+    plt.plot(x, y, label=label)
+
 def display_transformations(image_path, dest, options):
 
-    # Read the image
+    # pcv.params.debug = "plot"
+
     image, path, name = pcv.readimage(image_path)
-
-    # Display the original image
-    pcv.plot_image(image)
-
-    #  We want to remove as much background as possible without loosing any information about the plant
-
-    # Convert the image to grayscale by extracting the saturation channel
-    s = pcv.rgb2gray_hsv(
-        rgb_img=image,
-        channel='s'
-    )
-    pcv.plot_image(s)
-
-    # Threshold the grayscaled image
-    s_thresh = pcv.threshold.binary(
-        gray_img=s,
-        threshold=60,
-        object_type='light'
-    )
-    pcv.plot_image(s_thresh)
-
-
-
-
-
-
-
-def main():
-
-
-
-    # for key, value in transformations.items():
-    #     print(key)
-    #     transformed_image = value(image)
-    #     pcv.plot_image(transformed_image)
-    path, dest, options = parse_argument()
-
-    image, path, name = pcv.readimage(path)
     s = pcv.rgb2gray_hsv(rgb_img=image, channel='s')
     pcv.plot_image(s)
     s_thresh = pcv.threshold.binary(gray_img=s, threshold=85, object_type='light')
     #pcv.plot_image(s_thresh)
     s_mblur = pcv.median_blur(gray_img=s_thresh, ksize=5)
-    #pcv.plot_image(s_mblur)
+    pcv.plot_image(s_mblur)
     gaussian_bluri = pcv.gaussian_blur(img=s_thresh, ksize=(3, 3), sigma_x=0, sigma_y=None)
     #pcv.plot_image(gaussian_bluri)
 
     b = pcv.rgb2gray_lab(rgb_img=image, channel='b')
-    #pcv.plot_image(b)
+    pcv.plot_image(b)
 
     b_thresh=pcv.threshold.binary(gray_img=b, threshold=135,object_type='light')
 
     bs = pcv.logical_or(bin_img1=s_mblur, bin_img2=b_thresh)
-    #pcv.plot_image(bs)
+    pcv.plot_image(bs)
 
     masked = pcv.apply_mask(img=image, mask=bs, mask_color='white')
-    #pcv.plot_image(masked)
+    pcv.plot_image(masked)
 
     masked_a = pcv.rgb2gray_lab(rgb_img=masked, channel='a')
     masked_b = pcv.rgb2gray_lab(rgb_img=masked, channel='b')
 
-    #pcv.plot_image(masked_a)
+    pcv.plot_image(masked_a)
     #pcv.plot_image(masked_b)
 
     maskeda_thresh = pcv.threshold.binary(gray_img=masked_a, threshold=115, object_type='dark')
@@ -180,49 +153,111 @@ def main():
 
     ab_fill = pcv.fill(bin_img=ab, size=200)
     print("fill")
-    pcv.plot_image(ab_fill)
+    # pcv.plot_image(ab_fill)
     #pcv.plot_image(ab_fill)
     closed_ab = pcv.closing(gray_img=ab_fill)
     #pcv.plot_image(gray_img=ab_fill)
     masked2= pcv.apply_mask(img=masked, mask=ab_fill, mask_color='white')
-    pcv.plot_image(masked2)
+    # pcv.plot_image(masked2)
 
     #obj_hierachy= pcv.find_objects(img=masked2, mask=ab_fill)
 
     #roi1, roi_hierarchy = pcv.roi.rectangle(img=masked2, x=(image.shape[0] * 0.1), y=(image.shape[1] * 0.1), w=(image.shape[0] * 0.9), h=(image.shape[1] * 0.9))
     roi= pcv.roi.rectangle(img=masked2, x=0,y=0, w=image.shape[0], h=image.shape[1])
     kept_mask = pcv.roi.filter(mask=ab_fill, roi=roi, roi_type='partial')
-    #pcv.plot_image(filtered)
+    #pcv.plot_image(filtered)`
     analysis_image = pcv.analyze.size(img=image, labeled_mask=kept_mask)
     print("analysis")
-    pcv.plot_image(analysis_image)
+    # pcv.plot_image(analysis_image)
+    #print(vars(pcv))
+
+    print("DTYPE:", bs.dtype)
+
+    import cv2
+
+    import numpy as np
+
+    # Create a new array, filled with the 2 first dimensions of image
+
+    arr = np.zeros((masked2.shape[0], masked2.shape[1]), dtype=np.uint8)
+    for i in range(masked2.shape[0]):
+        for j in range(masked2.shape[1]):
+            arr[i][j] = masked2[i][j][1]
+    print("SAHPE :", arr.shape)
+
+    img, _, _ = pcv.readimage(image_path)
+    top_x, bottom_x, center_v_x = pcv.homology.x_axis_pseudolandmarks(img=analysis_image, mask=arr, label='default')
+    top_y, bottom_y, center_v_y = pcv.homology.y_axis_pseudolandmarks(img=analysis_image, mask=arr, label='default')
+
+    print("TOP X:", top_x)
+    print("TOP Y:", top_y)
+
+    plt.imshow(arr, cmap='gray')
+
+    for i in range(len(top_x)):
+        plt.scatter(top_x[i][0][0], top_x[i][0][1], c='r', s=10)
+
+    for i in range(len(top_y)):
+        plt.scatter(top_y[i][0][0], top_y[i][0][1], c='r', s=10)
+
+    for i in range(len(bottom_x)):
+        plt.scatter(bottom_x[i][0][0], bottom_x[i][0][1], c='b', s=10)
+
+    for i in range(len(bottom_y)):
+        plt.scatter(bottom_y[i][0][0], bottom_y[i][0][1], c='b', s=10)
+
+    for i in range(len(center_v_x)):
+        plt.scatter(center_v_x[i][0][0], center_v_x[i][0][1], c='g', s=10)
+
+    for i in range(len(center_v_y)):
+        plt.scatter(center_v_y[i][0][0], center_v_y[i][0][1], c='g', s=10)
+
+
+    # for i in range(len(bottom)):
+        # plt.scatter(bottom[i][0][0], bottom[i][0][1], c='b', s=10)
+#
+    # for i in range(len(center_v)):
+        # plt.scatter(center_v[i][0][0], center_v[i][0][1], c='g', s=10)
+#
+    plt.show()
+
+
+
 
 
     color_histogram = pcv.analyze.color(rgb_img=image, colorspaces="all", labeled_mask=kept_mask,label="default")
-    #pcv.plot_image(color_histogram)
     #print(color_histogram)
     res = pcv.outputs.save_results("histo")
     #print(list(pcv.outputs.observations['default_1']['saturation_frequencies'].keys()))
     print(pcv.outputs.observations['default_1']['blue_frequencies']['value'])
-    print(pcv.outputs.observations['default_1']['blue_frequencies']['label'])
+    print(f"SUM = {sum(pcv.outputs.observations['default_1']['lightness_frequencies']['value'])}")
 
-    y = pcv.outputs.observations['default_1']['blue_frequencies']['value']
-    x = pcv.outputs.observations['default_1']['blue_frequencies']['label']
 
-    plt.plot(x, y, label="blue")
+    dict_label = {"blue_frequencies" : 1, "green_frequencies": 1, "green-magenta_frequencies" : 1 , "lightness_frequencies" : 2.55, "red_frequencies" : 1, "blue-yellow_frequencies" : 1, "hue_frequencies" : 1, "saturation_frequencies" : 2.55, "value_frequencies" : 2.55}
 
-    y = pcv.outputs.observations['default_1']['green_frequencies']['value']
-    x = pcv.outputs.observations['default_1']['green_frequencies']['label']
-    plt.plot(x, y, label="green")
-    y = pcv.outputs.observations['default_1']['red_frequencies']['value']
-    x = pcv.outputs.observations['default_1']['red_frequencies']['label']
-    plt.plot(x, y, label="red")
+    for key, val in dict_label.items():
+        plot_stat_hist(key, val)
     plt.legend()
     plt.show()
     hue_cir = pcv.outputs.observations['default_1']['hue_frequencies']
     print(list(hue_cir.keys()))
     #print(hue_cir)
     pcv.print_image(img=color_histogram, filename="histo.png")
+
+
+    homolog_pts, start_pts, stop_pts, ptvals, chain, max_dist = pcv.homology.acute(img=img,
+                                                                               mask=arr, win=42,
+                                                                               threshold=130)
+    img_plms = image.copy()
+    cv2.drawContours(img_plms, homolog_pts, -1, (255, 255, 255), pcv.params.line_thickness)
+
+
+    pcv.plot_image(img_plms)
+    plm_fig=plt.figure(figsize=(7, 10))
+    plm_fig=plt.imshow(img_plms)
+    plm_fig=plt.xscale('linear')
+    plm_fig=plt.axis('off')
+    plt.show()
 
 
 
