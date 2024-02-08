8bc43109c0efb50f98dd304863957fecf7ec5c17
Charles Mariot (42) <contact@charles-mariot.fr>
2024-02-08 17:26:05 +0100

    Todo: get_filename / create directory in transformation when the path is a dir

--- transformation/Transformation.py
+++ transformation/Transformation.py
@@ -1,10 +1,11 @@
-# https://plantcv.readthedocs.io/en/v3.4.1/vis_tutorial/
-
 import argparse
 from plantcv import plantcv as pcv
 import os
 import matplotlib.pyplot as plt
 import cv2
+from rembg import remove
+from tqdm import tqdm
+import numpy as np
 
 
 def parse_argument() -> str:
@@ -18,7 +19,8 @@ def parse_argument() -> str:
         prog='Transformation.py',
         description="""
         If the path is a file, display the transformation of the image.
-        If the path is a directory, display the transformation of all the images in the directory and save them in the 'dest' directory.
+        If the path is a directory, display the transformation of all the images
+        in the directory and save them in the 'dest' directory.
         """
     )
 
@@ -31,122 +33,134 @@ def parse_argument() -> str:
     parser.add_argument(
         '-dst',
         type=str,
-        default='.',
+        default='../transformed_directory',
         help='Destination of the transformed image',
     )
 
     # Blur
     parser.add_argument(
-        '-blur',
-        type=bool,
-        default=True,
+        '-blur', '-b',
+        default=False,
+        action='store_true',
         help='Do not display/save the blur the image',
     )
 
     # Mask
     parser.add_argument(
-        '-mask',
-        type=bool,
-        default=True,
+        '-mask', '-m',
+        default=False,
+        action='store_true',
         help='Do not display/save the mask of the transformed image',
     )
 
     # ROI
     parser.add_argument(
-        '-roi',
-        type=bool,
-        default=True,
+        '-roi', '-r',
+        default=False,
+        action='store_true',
         help='Do not display/save the ROI of the transformed image',
     )
 
     # Object
     parser.add_argument(
-        '-obj',
-        type=bool,
-        default=True,
+        '-obj', '-o',
+        default=False,
+        action='store_true',
         help='Do not display/save the object of the transformed image',
     )
 
     # Pseudolandmark
     parser.add_argument(
-        '-pseudo',
-        type=bool,
-        default=True,
+        '-pseudo', '-p',
+        default=False,
+        action='store_true',
         help='Do not display/save the pseudolandmark of the transformed image',
     )
 
     args = parser.parse_args()
 
-    options = [args.blur, args.mask, args.roi, args.obj, args.pseudo]
+    options = np.array([
+        args.blur,
+        args.mask,
+        args.roi,
+        args.obj,
+        args.pseudo
+    ])
+
+    image_to_plot = np.array([
+        "Gaussian blur",
+        "Mask",
+        "ROI Objects",
+        "Analyze object",
+        "Pseudolandmarks"
+    ])
 
     return (
         args.path,
         args.dst,
-        options
+        image_to_plot[options] if options.any() else image_to_plot
     )
 
-def total_luminosity(image):
-    tot = 0
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            tot = tot + image[i][j]
-    return tot
 
-def plot_stat_hist(label, sc=1):
+def is_roi_border(x, y, roi_start_x, roi_start_y, roi_h, roi_w, roi_line_w):
+    """
+    Return true if the pixel in position x, y is the border of the rectangle
+    defined by the roi parameters.
+    The contour is the line of the rectangle, with a width of roi_line_w.
 
-    y = pcv.outputs.observations['default_1'][label]['value']
-    x = [i * sc for i in  pcv.outputs.observations['default_1'][label]['label']]
+    :param x: The x position of the pixel
+    :param y: The y position of the pixel
+    :param roi_start_x: The x position of the roi rectangle start
+    :param roi_start_y: The y position of the roi rectangle start
+    :param roi_h: The height of the roi rectangle
+    :param roi_w: The width of the roi rectangle
+    :param roi_line_w: The width of the roi rectangle line
+    """
 
-    if label == "hue_frequencies":
-        x = x[:int(255 / 2)]
-        y = y[:int(255 / 2)]
-    if label == "blue-yellow_frequencies" or label == "green-magenta_frequencies":
-        x = [x + 128 for x in x]
+    return (
+        (
+            roi_start_x <= x <= roi_start_x + roi_w and
+            roi_start_y <= y <= roi_start_y + roi_line_w
+        )
+            or
+        (
+            roi_start_x <= x <= roi_start_x + roi_w and
+            roi_start_y + roi_h - roi_line_w <= y <= roi_start_y + roi_h
+        )
+            or
+        (
+            roi_start_x <= x <= roi_start_x + roi_line_w and
+            roi_start_y <= y <= roi_start_y + roi_h
+        )
+            or
+        (
+            roi_start_x + roi_w - roi_line_w <= x <= roi_start_x + roi_w and
+            roi_start_y <= y <= roi_start_y + roi_h
+        )
+    )
 
-    plt.plot(x, y, label=label)
 
-def determine_threshold(b):
-    b_thresh=pcv.threshold.binary(gray_img=b, threshold=50, max_value=255, object_type='light')
-    past_lum = total_luminosity(b_thresh)
-    max_diff = 0
-    iter = 0
-    for loop in range(50, 250, 2):
-        b_thresh=pcv.threshold.binary(gray_img=b, threshold=loop, max_value=255, object_type='light')
-        #pcv.plot_image(b_thresh)
-        cur_lum = total_luminosity(b_thresh)
-        diff = past_lum - cur_lum
+def create_roi_image(image, mask, roi_start_x, roi_start_y, roi_h, roi_w, roi_line_w):
 
-        if max_diff < diff:
-            max_diff = diff
-        #print("iter", loop, "lum", cur_lum, "delta", diff)
-        if (max_diff != diff):
-            iter = loop
-            break
-        past_lum = cur_lum
-    max_diff = diff
+    """
+    Create an image with the ROI rectangle and the mask
+    """
 
-    for loop in range(iter, 250, 1):
-        b_thresh=pcv.threshold.binary(gray_img=b, threshold=loop, max_value=255, object_type='light')
-        cur_lum = total_luminosity(b_thresh)
-        diff = past_lum - cur_lum
-        #print("iter", loop, "lum", cur_lum, "delta", diff)
-        if max_diff > diff:
-            max_diff = diff
-        if (max_diff != diff):
-            #print("this one")
-            return loop
-        past_lum = cur_lum
-    return 0
+    roi_image = image.copy()
+    roi_image[mask != 0] = (0, 255, 0)
+    for x in range(0, image.shape[0]):
+        for y in range(0, image.shape[1]):
+            if is_roi_border(x, y, roi_start_x, roi_start_y,
+                             roi_h, roi_w, roi_line_w):
+                roi_image[x, y] = (255, 0, 0)
+    return roi_image
 
-def gaussian_blurf(img, sat):
-    s = pcv.rgb2gray_hsv(rgb_img=img, channel='s')
-    s_thresh = pcv.threshold.binary(gray_img=s, threshold=sat, max_value=255, object_type='light')
-    gaussian_bluri = pcv.gaussian_blur(img=s_thresh, ksize=(5, 5), sigma_x=0, sigma_y=None)
-    return gaussian_bluri
 
-def plot_histogram(image, labeled_mask):
+def plot_histogram(image, kept_mask):
 
-    pcv.analyze.color(rgb_img=image, colorspaces="all", labeled_mask=labeled_mask,label="default")
+    """
+    Plot the histogram of the image
+    """
 
     def plot_stat_hist(label, sc=1):
         y = pcv.outputs.observations['default_1'][label]['value']
@@ -159,9 +173,6 @@ def plot_histogram(image, labeled_mask):
         plt.plot(x, y, label=label)
         return
 
-    plt.subplots(figsize=(16, 9))
-
-    print("plot histogram")
     dict_label = {
         "blue_frequencies" : 1,
         "green_frequencies": 1,
@@ -174,6 +185,15 @@ def plot_histogram(image, labeled_mask):
         "value_frequencies" : 2.55
     }
 
+    labels, _ = pcv.create_labels(mask=kept_mask)
+    pcv.analyze.color(
+        rgb_img=image,
+        colorspaces="all",
+        labeled_mask=labels,
+        label="default"
+    )
+
+    plt.subplots(figsize=(16, 9))
     for key, val in dict_label.items():
         plot_stat_hist(key, val)
 
@@ -190,124 +210,190 @@ def plot_histogram(image, labeled_mask):
     )
     plt.show()
 
-def display_transformations(image_path, dest, options):
 
-    #pcv.params.debug = "print"
+def get_image_name(image_path, dest, label, dir_name):
 
-    image, path, name = pcv.readimage(image_path)
-    #pcv.plot_image(image)
-    #print("original")
+    print(f"image_path {image_path}")
+    print(f"dir_name {dir_name}")
+    print(f"dest {dest}")
+    print(f"label {label}")
 
-    l = pcv.rgb2gray_lab(rgb_img=image, channel='l')
-    #pcv.plot_image(l)
-    l_thresh = pcv.threshold.binary(gray_img=l, threshold=30, max_value=255, object_type='light')
-    #pcv.plot_image(l_thresh)
+    new_image_path = image_path.replace(dir_name, dest + "/", 1)
 
-    b = pcv.rgb2gray_lab(rgb_img=image, channel='b')
-    #pcv.plot_image(b)
-    value_threshold = determine_threshold(b)
-    print("Value b threshold", determine_threshold(b))
-    # pcv.plot_image(pcv.threshold.binary(gray_img=b, threshold=116,object_type='light'))
-    b_thresh=pcv.threshold.binary(gray_img=b, threshold=value_threshold,max_value=255, object_type='light')
+    print(f"new_image_path {new_image_path}")
 
-    bminusl_thresh = pcv.apply_mask(img=b_thresh, mask=l_thresh, mask_color='black')
+    dirs = new_image_path.split("/")
 
-    masked = pcv.apply_mask(img=image, mask=bminusl_thresh, mask_color='white')
-    gaussian_bluri = gaussian_blurf(masked, 80)
+    for i in range(len(dirs) - 1):
+        dir_to_create = "/".join(dirs[:i + 1])
+        if not os.path.isdir(dir_to_create):
+            os.mkdir(dir_to_create)
 
-    #pcv.plot_image(gaussian_bluri)
-    #print("gaussian blur")
+    point_pos = image_path.rfind(".")
 
-    b_fill = pcv.fill(bin_img=bminusl_thresh, size=200)
-    print("fill")
+    if point_pos == -1:
+        filename_without_ext = image_path.replace(dir_name, dest, 1)
+        extension = ""
+    else:
+        filename_without_ext = image_path[:point_pos].replace(dir_name, dest, 1)
+        extension = image_path[point_pos:]
 
-    masked2= pcv.apply_mask(img=masked, mask=b_fill, mask_color='white')
-    print("mask")
-    #pcv.plot_image(masked2)
+    image_name = new_image_path.replace(
+        filename_without_ext,
+        f"{filename_without_ext}_{label}{extension}",
+        1
+    )
 
-    id_objects, obj_hierarchy = pcv.find_objects(masked2, b_fill)
+    exit()
 
-    roi, roi_hierarchy= pcv.roi.rectangle(img=masked2, x=0, y=0, w=image.shape[0], h=image.shape[1])
-
-    roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(img=image, roi_contour=roi,
-                                                                    roi_hierarchy=roi_hierarchy,
-                                                                    object_contour=id_objects,
-                                                                    obj_hierarchy=obj_hierarchy,
-                                                                    roi_type='partial')
-
-    obj, mask = pcv.object_composition(img=image, contours=roi_objects, hierarchy=hierarchy3)
-
-    # pcv.plot_image(mask)
-
-    shape_img = pcv.analyze_object(img=image, obj=obj, mask=mask, label="default")
-    # pcv.plot_image(shape_img)
-    boundary_img1 = pcv.analyze_bound_horizontal(img=image, obj=obj, mask=mask,
-                                                   line_position=1680, label="default")
-
-    pcv.plot_image(boundary_img1)
-    color_histogram = pcv.analyze_color(rgb_img=image, mask=mask, hist_plot_type='all', label="default")
-    pcv.plot_image(color_histogram)
-    gray_img = pcv.rgb2gray_hsv(rgb_img=image, channel='v')
-    pseudocolored_img = pcv.visualize.pseudocolor(gray_img=gray_img, mask=b_fill, cmap='jet')
-    pcv.plot_image(pseudocolored_img)
-    pcv.print_results(filename="histo_results.txt")
+    return image_name
 
 
 
+def transform_image(image_path, dest, options, is_launch_on_dir=False, dir_name=""):
 
-    # roi= pcv.roi.rectangle(img=masked2, x=0,y=0, w=image.shape[0], h=image.shape[1])
-    # kept_mask = pcv.roi.filter(mask=b_fill, roi=roi, roi_type='partial')
+    # Open the image with plantcv
+    image, _, _ = pcv.readimage(image_path, mode='rgb')
 
-    #pcv.plot_image(kept_mask)
+    # Remove the background of the image
+    image_without_bg = remove(image)
 
+    # Convert the image to grayscale
+    b = pcv.rgb2gray_lab(rgb_img = image_without_bg, channel='l')
 
-    # labelo, obj = pcv.create_labels(mask=kept_mask)
+    # Create a binary image with a threshold
+    b_thresh = pcv.threshold.binary(gray_img=b, threshold=35, object_type='light')
 
-    # analysis_image = pcv.analyze.size(img=image, labeled_mask=kept_mask)
-    #print("analye object")
-    #pcv.plot_image(analysis_image)
+    # Remove small objects from the binary image that are smaller than 200 pxls
+    filled = pcv.fill(bin_img=b_thresh, size=200)
 
-    _, ax = plt.subplots(ncols=3, nrows=2, figsize=(16, 9))
+    # Apply a gaussian blur to the image to remove the noise
+    gaussian_bluri = pcv.gaussian_blur(img=filled, ksize=(3, 3))
+
+    # Apply a mask to the image
+    masked = pcv.apply_mask(img=image, mask=gaussian_bluri, mask_color='black')
+
+    # Create a region of interest (ROI) rectangle
+    roi_start_x = 0
+    roi_start_y = 0
+    roi_w = image.shape[0]
+    roi_h = image.shape[1]
+    roi_line_w = 5
+    roi = pcv.roi.rectangle(
+        img=masked,
+        x=roi_start_x,
+        y=roi_start_y,
+        w=roi_w,
+        h=roi_h
+    )
+
+    # Create a mask based on the ROI
+    kept_mask = pcv.roi.filter(mask=filled, roi=roi, roi_type='partial')
+
+    # Create a displayable image with the ROI rectangle and the mask
+    roi_image = create_roi_image(
+        image, kept_mask, roi_start_x, roi_start_y, roi_h, roi_w, roi_line_w
+    )
+
+    # Analyze the object in the image
+    analysis_image = pcv.analyze.size(img=image, labeled_mask=kept_mask)
+
     images_to_plot = {
         "Original": cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
         "Gaussian blur": cv2.cvtColor(gaussian_bluri, cv2.COLOR_BGR2RGB),
-        "Mask": cv2.cvtColor(masked2, cv2.COLOR_BGR2RGB),
-        "ROI Objects": labelo,
+        "Mask": cv2.cvtColor(masked, cv2.COLOR_BGR2RGB),
+        "ROI Objects": cv2.cvtColor(roi_image, cv2.COLOR_BGR2RGB),
         "Analyze object": cv2.cvtColor(analysis_image, cv2.COLOR_BGR2RGB),
         "Pseudolandmarks": cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     }
 
-    for (label, img), axe in zip(images_to_plot.items(), ax.flat):
-        axe.imshow(img)
-        axe.set_title(label)
-        axe.set(xticks=[], yticks=[])
-        axe.label_outer()
+    # If the argument of the program is a file, display the transformation
+    if not is_launch_on_dir:
 
-    #print("pseudolandmarks")
-    #plt.imshow(mplimg.imread(image_path))
-    top_x, bottom_x, center_v_x = pcv.homology.x_axis_pseudolandmarks(img=image, mask=b_fill, label='default')
+        fig, ax = plt.subplots(ncols=3, nrows=2, figsize=(16, 9))
 
-    for i in range(len(top_x)):
-        plt.scatter(top_x[i][0][0], top_x[i][0][1], c='r', s=10)
+        fig.suptitle(f"Transformation of {image_path}")
 
-    for i in range(len(bottom_x)):
-        plt.scatter(bottom_x[i][0][0], bottom_x[i][0][1], c='b', s=10)
+        for (label, img), axe in zip(images_to_plot.items(), ax.flat):
+            axe.imshow(img)
+            axe.set_title(label)
+            axe.set(xticks=[], yticks=[])
+            axe.label_outer()
 
-    for i in range(len(center_v_x)):
-        plt.scatter(center_v_x[i][0][0], center_v_x[i][0][1], c='y', s=10)
-    plt.show()
+        top_x, bottom_x, center_v_x = pcv.homology.x_axis_pseudolandmarks(img=image, mask=filled, label='default')
 
-    plot_histogram(image, kept_mask)
+        for i in range(len(top_x)):
+            if len(top_x[i]) >= 1 and len(top_x[i][0]) >= 2:
+                plt.scatter(top_x[i][0][0], top_x[i][0][1], c='blue', s=10)
+        for i in range(len(bottom_x)):
+            if len(bottom_x[i]) >= 1 and len(bottom_x[i][0]) >= 2:
+                plt.scatter(bottom_x[i][0][0], bottom_x[i][0][1], c='magenta', s=10)
+        for i in range(len(center_v_x)):
+            if len(center_v_x[i]) >= 1 and len(center_v_x[i][0]) >= 2:
+                plt.scatter(center_v_x[i][0][0], center_v_x[i][0][1], c='orange', s=10)
+
+        plt.show()
+        plot_histogram(image, kept_mask)
+        plt.close()
+
+    # Else if the argument of the program is a directory, save the transformation
+    else:
+
+        for label, img in images_to_plot.items():
+            if label in options:
+                image_name = get_image_name(image_path, dest, label, dir_name)
+                # cv2.imwrite(image_name, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+                print(f"Image saved at {image_name}")
+
+
+def get_image_list(path: str) -> list:
+
+    """
+    Return the list of files in the directory associated with 'path'
+    It walks in the subdirectories
+    """
+
+    image_list = []
+    for root, _, files in os.walk(path):
+        for file in files:
+            full_path = os.path.join(root, file)
+            image_list.append(full_path)
+    return image_list
+
+
+def transform_directory(path, dest, options):
+
+    """
+    Transform all the images in the directory 'path' and save them in the
+    'dest' directory
+    """
+
+    images_list: list = get_image_list(path)
+    for image in tqdm(images_list):
+        transform_image(
+            image,
+            dest,
+            options,
+            is_launch_on_dir=True,
+            dir_name=path
+        )
+
 
 def main():
+
+    """
+    Main function of the program,
+    it calls the other functions depending on the path
+    - If the path is a file, display the transformation of the image.
+    - If the path is a directory, save the transformation of all the images
+    """
 
     path, dest, options = parse_argument()
 
     if os.path.isfile(path):
-        display_transformations(path, dest, options)
+        transform_image(path, dest, options)
     elif os.path.isdir(path):
-        # transform_directory(path, dest, options)
-        pass
+        transform_directory(path, dest, options)
     else:
         raise Exception("The path is not a file or a directory")
 
@@ -316,7 +402,7 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print()
+        print("\nSIGTERM")
         exit()
     except Exception as error:
         print(error)
