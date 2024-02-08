53fa30aa65b93ff14c0cd3bf66b747ad6b2bcf79
Charles Mariot (42) <contact@charles-mariot.fr>
2024-02-08 12:04:40 +0100

    RGB image for the readme, instead of the BGR

--- analysis/image_grid.py
+++ analysis/image_grid.py
@@ -1,4 +1,3 @@
-import cv2 as cv
 import matplotlib.pyplot as plt
 
 
@@ -12,14 +11,14 @@ def main():
     _, axs = plt.subplots(2, 4)
 
     images = {
-        "Apple healthy": cv.imread("../images/Apple_healthy/image (1).JPG"),
-        "Apple Black rot": cv.imread("../images/Apple_Black_rot/image (1).JPG"),
-        "Apple rust": cv.imread("../images/Apple_rust/image (1).JPG"),
-        "Apple scab": cv.imread("../images/Apple_scab/image (1).JPG"),
-        "Grape healthy": cv.imread("../images/Grape_healthy/image (1).JPG"),
-        "Grape Black rot": cv.imread("../images/Grape_Black_rot/image (1).JPG"),
-        "Grape Esca": cv.imread("../images/Grape_Esca/image (1).JPG"),
-        "Grape spot": cv.imread("../images/Grape_spot/image (1).JPG")
+        "Apple healthy": plt.imread("../images/Apple_healthy/image (1).JPG"),
+        "Apple Black rot": plt.imread("../images/Apple_Black_rot/image (1).JPG"),
+        "Apple rust": plt.imread("../images/Apple_rust/image (1).JPG"),
+        "Apple scab": plt.imread("../images/Apple_scab/image (1).JPG"),
+        "Grape healthy": plt.imread("../images/Grape_healthy/image (1).JPG"),
+        "Grape Black rot": plt.imread("../images/Grape_Black_rot/image (1).JPG"),
+        "Grape Esca": plt.imread("../images/Grape_Esca/image (1).JPG"),
+        "Grape spot": plt.imread("../images/Grape_spot/image (1).JPG")
     }
 
     for ax, (label, img) in zip(axs.flat, images.items()):
